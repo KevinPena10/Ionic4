11569687605d0e3aa9219729a79be85db7fecfc6
KevinStivenP <kevin.pena@accenture.com>
2021-04-14 22:26:02 -0500

    Agregando nuevos Slides para la aplicación y notas

--- Platzi-Music/src/app/home/home.page.ts
+++ Platzi-Music/src/app/home/home.page.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slideOps = {
+    initialSlide: 0,
+    slidesPerView:1,
+    centeredSlides: true,
+    speed: 400
+  };
+
   constructor() {}
 
 }
