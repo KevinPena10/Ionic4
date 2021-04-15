66fb557a2638cc92814d86bc53d2242c265b9146
KevinStivenP <kevin.pena@accenture.com>
2021-04-15 08:37:04 -0500

    Working with Ionic Storage and Adding a intro component

--- Platzi-Music/src/app/home/home.page.ts
+++ Platzi-Music/src/app/home/home.page.ts
@@ -6,35 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  slideOpt = {
-    initialSlide: 0,
-    slidesPerView:1,
-    centeredSlides: true,
-    speed: 400
-  };
-
-  slides = [
-    {
-    title:"Escucha tu música", 
-    subTitle:"En CUALQUIER LUGAR", 
-    description:"Los mejors álbumes, las mejores Canciones. Escucha y Comparte en cualquier momento, a todas horas.",
-    icon:"Play",
-    },
-    {
-      title:"Disfruta de nuestro reproductor",
-      subTitle:"DE VIDEOS INCREÍBLES",
-      description:"Entra al modo video de nuestro reproductor y obtén acceso a clips, documentales y making offs increbles de tus artita favorito.",
-      icon:"videocam",
-    }
-    ,
-    {
-      title:"Accede al exclusivo",
-      subTitle:"MODO DEPORTE",
-      description:" Crea una playlist basada en tu actividad física. Ten reportes y acceso a lo que necesites, integrado con GPS!",
-      icon:"bicycle",
-    }]
-
   constructor() {}
 
 }
