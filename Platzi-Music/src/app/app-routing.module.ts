758c25a3e77494a4ed9b6d15f5d1c5f58e86e9fc
Kevin Peña <kevin.stiven.pena@gmail.com>
2021-04-18 11:02:53 -0500

    Working with IONIC Css utilities

--- Platzi-Music/src/app/app-routing.module.ts
+++ Platzi-Music/src/app/app-routing.module.ts
@@ -16,7 +16,11 @@ const routes: Routes = [
   {
     path: 'intro',
     loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+
 ];
 
 @NgModule({
