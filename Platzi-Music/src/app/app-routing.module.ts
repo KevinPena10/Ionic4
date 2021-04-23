b87b9d12218324b1e330ef9372d7af8e463fa79d
Kevin Peña <kevin.stiven.pena@gmail.com>
2021-04-23 08:48:23 -0500

    Creating the menu in ionic

--- Platzi-Music/src/app/app-routing.module.ts
+++ Platzi-Music/src/app/app-routing.module.ts
@@ -4,11 +4,7 @@ import {IntroGuard} from './guards/intro.guard';
 import {LoginGuard} from './guards/login.guard';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
-    canActivate: [LoginGuard, IntroGuard], //Dos guards para el home
-  },
+
   {
     path: '',
     redirectTo: 'home',
@@ -21,9 +17,16 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },  {
+  },
+  {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule),
+    canActivate: [LoginGuard, IntroGuard], //Dos guards para el home
+
   },
 
 
