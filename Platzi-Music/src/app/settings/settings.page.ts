22a64b8bdd7383825333232e09efc818a647ce2b
Kevin Peña <kevin.stiven.pena@gmail.com>
2021-04-25 18:27:13 -0500

    Using the camera through Capacitor

--- Platzi-Music/src/app/settings/settings.page.ts
+++ Platzi-Music/src/app/settings/settings.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Plugins, CameraResultType, CameraSource } from "@capacitor/core";
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
 })
-export class SettingsPage implements OnInit {
-  userImage ="assets/img/user.jpg"
-  constructor() { }
+export class SettingsPage {
+  userImage ="assets/img/user.jpg";
+  photo: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer) { }
 
-  ngOnInit() {
+  async takePhoto(){
+   const image = await Plugins.Camera.getPhoto({
+     quality: 100,
+     allowEditing: false,
+     resultType:CameraResultType.DataUrl,
+     source: CameraSource.Camera
+   });
+  
+    this.photo =  this.sanitizer.bypassSecurityTrustResourceUrl(
+      image && image.dataUrl
+    );
+  
   }
 
 }
